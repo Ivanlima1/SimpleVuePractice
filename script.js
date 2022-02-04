38aabdbaf9d5bd4a28144234928c6e46a4f4a952
José Ivan de Lima Silva <i.jose66@hotmail.com>
2022-02-04 10:55:50 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,15 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        teste: 'Que bacana!'
+    },
+    el: '#app2',
+    data: {
+        inputUsuario: ''
+    },
+    el: '#app3',
+    data: {
+        mostrar: true
+    }
+})
+
