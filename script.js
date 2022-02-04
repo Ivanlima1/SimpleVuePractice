3a7abf018bc03de70179c3d9a06929ec15fbde17
José Ivan de Lima Silva <i.jose66@hotmail.com>
2022-02-04 14:21:52 -0300

    Novas alterações

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 var app = new Vue({
     el: '#app',
     data: {
-        teste: 'Que bacana!'
-    },
-    el: '#app2',
-    data: {
-        inputUsuario: ''
-    },
-    el: '#app3',
-    data: {
-        mostrar: true
+        //Exemplo V-bind
+        teste: 'Que bacana!',
+        //Exemplo V-model
+        inputUsuario: '',
+        //Exemplo V-if
+        mostrar: 'Esconder',
+        //Exemplo V-for
+        lista: [{nome: 'craque', idade:'22'}, {nome: 'maestro', idade: '34'}]
     }
 })
 
